dfb9925604d936038d326a5860e73a2e0504fd40
Kikkirej <5462557+Kikkirej@users.noreply.github.com>
2022-01-15 20:24:45 +0100

    🎉 initialize with start.spring.io

---
+++ src/main/java/net/kikkirej/alexandria/maven/module/identification/ModuleIdentificationApplication.java
@@ -0,0 +1,13 @@
+package net.kikkirej.alexandria.maven.module.identification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleIdentificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleIdentificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/kikkirej/alexandria/maven/module/identification/ModuleIdentificationApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kikkirej.alexandria.maven.module.identification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleIdentificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
