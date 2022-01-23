faf9988e5b1b6e34ce6501e75ba01de093704fe9
Kikkirej <5462557+Kikkirej@users.noreply.github.com>
2022-01-23 15:17:14 +0100

    💚

--- src/test/java/net/kikkirej/alexandria/module/identification/DefiningObjectFinderTest.java
+++ src/test/java/net/kikkirej/alexandria/module/identification/DefiningObjectFinderTest.java
@@ -4,23 +4,25 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DefiningObjectFinderTest {
 
     //SUT
-    private DefiningObjectFinder pomFinder;
+    private DefiningObjectFinder definingObjectFinder;
 
     @BeforeEach
     void setUp() {
-        pomFinder = new DefiningObjectFinder();
+        definingObjectFinder = new DefiningObjectFinder();
     }
 
     @Test
     void semicolonSeparatedPomDirectoriesIn() {
         File analysisDir = new File("./example-structure");
-        String result = pomFinder.moduleDirectoriesIn(analysisDir, "pom.xml");
+        Collection<String> result = definingObjectFinder.moduleDirectoriesIn(analysisDir, "pom.xml");
+        assertTrue(result.contains("/"));
         assertTrue(result.contains("/module1"));
         assertTrue(result.contains("/module2"));
         assertTrue(result.contains("/module2/submodule"));
